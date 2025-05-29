7fa26e297e4496307508575d52c5964ef04b0d7f
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2025-05-30 04:36:40 +0600

    vvvariable

--- Concepts of  variable/string/main.js
+++ Concepts of  variable/string/main.js
@@ -1,1 +1,40 @@
 console.log("checking");
+
+// working with string
+"hello" + "world";
+console.log("hello" + "world");
+console.log("hello".length); //length count
+console.log("hello vai"[7]); //index count
+
+console.log("hello 'parosh vai"); //escape
+
+console.log(`hello 'parosh' vai and "punom" vai`); //template string
+
+//working with number
+
+console.log(2 + 2);
+console.log(5 - 2);
+console.log(8 * 2);
+console.log(8 / 2);
+
+// modulus
+console.log(8 % 2);
+console.log(7 % 2);
+
+//exponential operation
+console.log(2 ** 3); // 2^3 = 2*2*2
+console.log(3 ** 3); // 3^3 = 3*3*3
+
+// work on variable
+var firstName = "muhaiminul";
+console.log(firstName + " sarkar");
+
+let lastName = "sarkar";
+console.log(lastName);
+
+console.log(firstName + " " + lastName);
+
+lastName = "parosh"; // here changing the value
+console.log(firstName + " " + lastName);
+
+const profession = " developers";
