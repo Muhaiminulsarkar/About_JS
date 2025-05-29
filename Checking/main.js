97666980015826c7d794134476c6c7778db9599b
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2025-05-29 23:39:18 +0600

    first commit

---
+++ Checking/main.js
@@ -0,0 +1,9 @@
+console.log(2 + 4);
+console.log(2 * 4);
+
+console.log(typeof(3));
+console.log(typeof('parosh'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(123n));
